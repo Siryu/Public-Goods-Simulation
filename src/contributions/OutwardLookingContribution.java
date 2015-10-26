7c1dc9ab242bde92b65c1b93e271b7d603a06b2f
Siryu <coreymassey@gmail.com>
2015-10-25 23:06:21 -0600

    add comments of thoughts into program

--- src/contributions/OutwardLookingContribution.java
+++ src/contributions/OutwardLookingContribution.java
@@ -12,13 +12,18 @@ public class OutwardLookingContribution extends Contribution{
 	public void adjustBetPercent(double reward, double bank, List<Contribution> otherContributions) {
 		double originalContribution = this.getBet(bank);
 		float currentGainChangePercent = (float) (reward / originalContribution);
-		this.previousChangePercent = currentGainChangePercent;
-		
-		for(Contribution oc : otherContributions) {
-			if(oc.getPreviousGainPercent() > currentGainChangePercent) {
-				this.directionIncreaseBet = oc.getBetPercent() > this.getBetPercent();	
+		// i divide by .00001 instead of 0 so the numbers are hugely skewed when checking if 0 is better....
+		if(currentGainChangePercent < this.previousChangePercent) {
+			for(Contribution oc : otherContributions) {
+				
+				// need to check oc's previous compared to their now and see if it's a positive increase and follow it.
+				
+			this.directionIncreaseBet = oc.getBetPercent() > this.getBetPercent();	
 			}
 		}
+		this.previousChangePercent = currentGainChangePercent;
+		
+	
 		
 		float directionValue = directionIncreaseBet ? 1 : -1;	
 		this.betPercent += (adjustRate * directionValue);
